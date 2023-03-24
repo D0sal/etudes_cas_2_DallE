7b6fed4cec4122bdc1a1e486c94e2feb838c3560
D0sal <nathan.oliveira.da.silva.pro@gmail.com>
2023-03-24 12:36:25 +0100

    added generate multiple images feature

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const express = require('express');
-
+const tf = require('@tensorflow/tfjs-core');
 const app = express();
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/:textInput', (req, res) => {
+app.get('/single/:textInput', async (req, res) => {
   const textInput = req.params.textInput;
-  // ici load les images avec tensorflow.js
-  
 
-  res.send({image: textInput})
+  // Loading model
+  /*
+  const model = await tf.loadLayersModel('file://path/to/my-model/model.json');
+  const output = model.predict(input);
+
+  res.send(output) <-- à convertir en format JSON
+  */
+ 
+  res.send({inputText: textInput})
+});
+
+app.get('/multiple/:textInput', async (req, res) => {
+  const textInput = req.params.textInput;
+
+  // Loading model
+  /*
+  const model = await tf.loadLayersModel('file://path/to/my-model/model.json');
+  const output = model.predict(input);
+
+  res.send(output) <-- à convertir en format JSON
+  */
+ 
+  res.send({inputText: textInput})
 });
 
 app.listen(3000, () => {
