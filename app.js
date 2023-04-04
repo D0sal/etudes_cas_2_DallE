92ecdf42c832173ceb06eab26c4d3008e0198715
D0sal <oliveirada@eisti.eu>
2023-04-04 16:13:39 +0200

    new mystic way to run python script

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const app = express();
+const { spawn } = require('child_process');
+
+const runPythonScript = (res, inputText) => {
+  const pythonProcess = spawn('python3', ['hello.py', inputText]);
+
+  pythonProcess.stdout.on('data', (data) => {
+    console.log('data returned', data.toString())
+    res.send(
+      JSON.stringify(`{${data.toString()}}`)
+      )
+  });
+  
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+    throw new Error('problem while running python script')
+  });
+  
+  pythonProcess.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -7,8 +28,8 @@ app.get('/', (req, res) => {
 
 app.get('/single/:textInput', async (req, res) => {
   const textInput = req.params.textInput;
- 
-  res.send({inputText: textInput})
+
+  runPythonScript(res, textInput);
 });
 
 app.get('/multiple/:textInput', async (req, res) => {
